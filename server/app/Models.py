21574a9f9d99f2987c930b00220ea335cf19eb8d
Rodolphe RAMAEL <rodolphe.ramael@epita.fr>
2017-11-27 12:32:17 +0100

    (feature) Add model implementation

---
+++ server/app/Models.py
@@ -0,0 +1,33 @@
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column, Integer, String, Boolean, ForeignKey
+from sqlalchemy.orm import sessionmaker, relationship
+from sqlalchemy import create_engine
+
+Base = declarative_base()
+
+
+class Todo(Base):
+    __tablename__ = 'todos'
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String, nullable=False, unique=True)
+    desc = Column(String)
+
+
+class Task(Base):
+    __tablename__ = 'tasks'
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String, nullable=False)
+    done = Column(Boolean, default=False)
+    desc = Column(String)
+    todo_id = Column(Integer, ForeignKey('todos.id'), nullable=False)
+
+    user = relationship('User')
+
+
+engine = create_engine('sqlite:///todo.db')
+Base.metadata.create_all(engine)
+Session = sessionmaker()
+Session.configure(bind=engine)
+session = Session()
